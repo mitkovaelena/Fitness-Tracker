ee2a046b5fb097a6f0446d8250f1c77d4c1e4516
pmpeev <pmpeev@users.noreply.github.com>
2016-08-08 13:18:06 +0300

    Cleanup and refactor CSS

--- scripts/register.js
+++ scripts/register.js
@@ -1,1 +1,8 @@
-importScripts("scripts/app.js");
+var loadFile = function(event) {
+    var reader = new FileReader();
+    reader.onload = function(){
+        var output = document.getElementById('output');
+        output.src = reader.result;
+    };
+    reader.readAsDataURL(event.target.files[0]);
+};
