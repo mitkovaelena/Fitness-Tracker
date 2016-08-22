f1e3c11a6166211a205d3901953090f0c63baba1
mitkovaelena <mitkova_elena@yahoo.com>
2016-08-22 12:08:21 +0300

    Revert "Revert "Login, Register funcionality  + Sidebar""
    This reverts commit 2860f60690e3d9d9b2c4986aae9467d479d551de.

--- scripts/login.js
+++ scripts/login.js
@@ -1,1 +1,29 @@
-importScripts("scripts/app.js");
+
+function login() {
+    const kinveyLoginUrl = kinveyBaseUrl + "user/" + kinveyAppKey + "/login";
+    const kinveyAuthHeaders = {'Authorization': 'Basic ' + btoa(kinveyAppKey + ":" + kinveyAppSecret)};
+
+    let username = $('#username').val();
+    let password = $('#password').val();
+
+    let loginData = {
+        username: username,
+        password: password
+    };
+
+    $.ajax({
+        type: "POST",
+        url: kinveyLoginUrl,
+        headers: kinveyAuthHeaders,
+        data: loginData,
+        success: loginSuccess,
+        error: handleAjaxError
+    });
+    function loginSuccess(data, status) {
+        showInfo('Login successful.');
+        sessionStorage.currentUser = JSON.stringify(data);
+        sessionStorage.authToken = data._kmd.authtoken;
+        sessionStorage.id = data._id;
+        window.location.replace("profile.html");
+    }
+}
