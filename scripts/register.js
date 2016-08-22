b4b7683268ac6107afdbf6a599b95ef0192b52da
mitkovaelena <mitkova_elena@yahoo.com>
2016-08-22 12:03:52 +0300

    Login, Register funcionality  + Sidebar

--- scripts/register.js
+++ scripts/register.js
@@ -6,3 +6,43 @@ var loadFile = function(event) {
     };
     reader.readAsDataURL(event.target.files[0]);
 };
+
+function register() {
+
+    const kinveyRegUrl = kinveyBaseUrl + "user/" + kinveyAppKey + "/";
+    const kinveyAuthHeaders = {'Authorization': 'Basic ' + btoa(kinveyAppKey + ":" + kinveyAppSecret)};
+
+    let username = $('#username').val();
+    let fullName = $('#full-name').val();
+    let gender = $("#gender").val();
+    let email = $('#email').val();
+    let trainer = $("#trainer").val();
+    let password = $('#password').val();
+
+
+    let data = {
+        username: username,
+        fullname: fullName,
+        gender: gender,
+        email: email,
+        trainerName: trainer,
+        password: password,
+        level: 1,
+        minutes: 0,
+        kg: 0,
+        km: 0
+    };
+
+    $.ajax({
+        type: "POST",
+        url: kinveyRegUrl,
+        headers: kinveyAuthHeaders,
+        data: data,
+        success: registerSuccess,
+        error: handleAjaxError
+    });
+    function registerSuccess(response) {
+        showInfo('Registration successful.');
+        window.location.replace("login.html");
+    }
+}
